4c79acb042aaa7a2483dfa51a4bf52d07dd0786c
Tom Schimansky <tomschimansk@gmail.com>
2022-04-11 15:36:34 +0200

    added pywin32 requirement for Windows

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(name="tkintermapview",
       classifiers=["Operating System :: OS Independent",
                    "Programming Language :: Python :: 3",
                    "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"],
-      install_requires=["geocoder", "pillow", "requests", "xerox"],
+      install_requires=["geocoder", "pillow", "requests", "xerox", 'pywin32; platform_system=="Windows"'],
       python_requires=">=3.6")
